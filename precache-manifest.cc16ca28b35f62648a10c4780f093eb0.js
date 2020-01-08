976d01fe2b3951ad1b9e4039a3ad386e11a2a704
samirsilwal <samirsilwal2424@gmail.com>
2020-01-08 09:42:47 +0545

    Updates

---
+++ precache-manifest.cc16ca28b35f62648a10c4780f093eb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d08be48c736d9d3ffda2ece4393b662b",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "dd1cbb74f0654b017a65",
+    "url": "/react-demo/static/css/main.4b5818a9.chunk.css"
+  },
+  {
+    "revision": "bee8205503ee7345fb12",
+    "url": "/react-demo/static/js/2.61aa4099.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-demo/static/js/2.61aa4099.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd1cbb74f0654b017a65",
+    "url": "/react-demo/static/js/main.ae743110.chunk.js"
+  },
+  {
+    "revision": "11bab4a465bdfd32b5d5",
+    "url": "/react-demo/static/js/runtime-main.7d508563.js"
+  }
+]);
